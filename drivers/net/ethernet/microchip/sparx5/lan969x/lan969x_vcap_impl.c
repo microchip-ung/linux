5992af29c8638ded7b25847743cf65b180926e9b
Daniel Machon <daniel.machon@microchip.com>
2023-11-28 15:06:01 +0100

    net: lan969x: merge upstream and local lan969x VCAP code

--- drivers/net/ethernet/microchip/sparx5/lan969x/lan969x_vcap_impl.c
+++ drivers/net/ethernet/microchip/sparx5/lan969x/lan969x_vcap_impl.c
@@ -5,44 +5,6 @@
 
 const struct sparx5_vcap_inst lan969x_vcap_inst_cfg[] = {
 	{
-		.vtype = VCAP_TYPE_ES0,
-		.lookups = SPARX5_ES0_LOOKUPS,
-		.lookups_per_instance = SPARX5_ES0_LOOKUPS,
-		.first_cid = SPARX5_VCAP_CID_ES0_L0,
-		.last_cid = SPARX5_VCAP_CID_ES0_MAX,
-		.count = 1536,
-	},
-	{
-		.vtype = VCAP_TYPE_ES2,
-		.lookups = SPARX5_ES2_LOOKUPS,
-		.lookups_per_instance = SPARX5_ES2_LOOKUPS,
-		.first_cid = SPARX5_VCAP_CID_ES2_L0,
-		.last_cid = SPARX5_VCAP_CID_ES2_MAX,
-		.count = 1024,
-	},
-	{
-		.vtype = VCAP_TYPE_IS2, /* IS2-0 */
-		.vinst = 0,
-		.map_id = 4,
-		.lookups = SPARX5_IS2_LOOKUPS,
-		.lookups_per_instance = SPARX5_IS2_LOOKUPS / 2,
-		.first_cid = SPARX5_VCAP_CID_IS2_L0,
-		.last_cid = SPARX5_VCAP_CID_IS2_L2 - 1,
-		.blockno = 0,
-		.blocks = 2,
-	},
-	{
-		.vtype = VCAP_TYPE_IS2, /* IS2-1 */
-		.vinst = 1,
-		.map_id = 5,
-		.lookups = SPARX5_IS2_LOOKUPS,
-		.lookups_per_instance = SPARX5_IS2_LOOKUPS / 2,
-		.first_cid = SPARX5_VCAP_CID_IS2_L2,
-		.last_cid = SPARX5_VCAP_CID_IS2_MAX,
-		.blockno = 2,
-		.blocks = 1,
-	},
-	{
 		.vtype = VCAP_TYPE_IS0, /* CLM-0 */
 		.vinst = 0,
 		.map_id = 1,
@@ -52,6 +14,7 @@ const struct sparx5_vcap_inst lan969x_vcap_inst_cfg[] = {
 		.last_cid = SPARX5_VCAP_CID_IS0_L2 - 1,
 		.blockno = 3,
 		.blocks = 1,
+		.ingress = true,
 	},
 	{
 		.vtype = VCAP_TYPE_IS0, /* CLM-1 */
@@ -63,6 +26,7 @@ const struct sparx5_vcap_inst lan969x_vcap_inst_cfg[] = {
 		.last_cid = SPARX5_VCAP_CID_IS0_L4 - 1,
 		.blockno = 4,
 		.blocks = 1,
+		.ingress = true,
 	},
 	{
 		.vtype = VCAP_TYPE_IS0, /* CLM-2 */
@@ -74,5 +38,48 @@ const struct sparx5_vcap_inst lan969x_vcap_inst_cfg[] = {
 		.last_cid = SPARX5_VCAP_CID_IS0_MAX,
 		.blockno = 5,
 		.blocks = 1,
+		.ingress = true,
+	},
+	{
+		.vtype = VCAP_TYPE_IS2, /* IS2-0 */
+		.vinst = 0,
+		.map_id = 4,
+		.lookups = SPARX5_IS2_LOOKUPS,
+		.lookups_per_instance = SPARX5_IS2_LOOKUPS / 2,
+		.first_cid = SPARX5_VCAP_CID_IS2_L0,
+		.last_cid = SPARX5_VCAP_CID_IS2_L2 - 1,
+		.blockno = 0,
+		.blocks = 2,
+		.ingress = true,
+	},
+	{
+		.vtype = VCAP_TYPE_IS2, /* IS2-1 */
+		.vinst = 1,
+		.map_id = 5,
+		.lookups = SPARX5_IS2_LOOKUPS,
+		.lookups_per_instance = SPARX5_IS2_LOOKUPS / 2,
+		.first_cid = SPARX5_VCAP_CID_IS2_L2,
+		.last_cid = SPARX5_VCAP_CID_IS2_MAX,
+		.blockno = 2,
+		.blocks = 1,
+		.ingress = true,
+	},
+	{
+		.vtype = VCAP_TYPE_ES0,
+		.lookups = SPARX5_ES0_LOOKUPS,
+		.lookups_per_instance = SPARX5_ES0_LOOKUPS,
+		.first_cid = SPARX5_VCAP_CID_ES0_L0,
+		.last_cid = SPARX5_VCAP_CID_ES0_MAX,
+		.count = 1536,
+		.ingress = false,
+	},
+	{
+		.vtype = VCAP_TYPE_ES2,
+		.lookups = SPARX5_ES2_LOOKUPS,
+		.lookups_per_instance = SPARX5_ES2_LOOKUPS,
+		.first_cid = SPARX5_VCAP_CID_ES2_L0,
+		.last_cid = SPARX5_VCAP_CID_ES2_MAX,
+		.count = 1024,
+		.ingress = false,
 	},
 };
