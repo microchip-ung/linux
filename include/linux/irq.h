b84c5fdf8df60cba4207838b2892a0676a528095
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-01-21 13:26:06 +0100

    Revert "genirq: Remove irq_chip_regs:: Polarity"
    This reverts commit 60029162a0458832ab2bcfc6fd4986bfd9ca0f55.
    
    As we are using the polarity, this register needs to be added back

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -991,6 +991,7 @@ void irq_init_desc(unsigned int irq);
  * @ack:	Ack register offset to reg_base
  * @eoi:	Eoi register offset to reg_base
  * @type:	Type configuration register offset to reg_base
+ * @polarity:	Polarity configuration register offset to reg_base
  */
 struct irq_chip_regs {
 	unsigned long		enable;
@@ -999,6 +1000,7 @@ struct irq_chip_regs {
 	unsigned long		ack;
 	unsigned long		eoi;
 	unsigned long		type;
+	unsigned long		polarity;
 };
 
 /**
