f7ae5d132148a4b66e236715f70a2e134a6455fb
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-01-23 15:01:15 +0100

    vcap: netlink: When adding u32 keys, use the correct mask
    By mistake for these keys as mask was used the value.

--- drivers/net/ethernet/microchip/vcap/vcap_netlink.c
+++ drivers/net/ethernet/microchip/vcap/vcap_netlink.c
@@ -318,7 +318,7 @@ static int vcap_genl_add_rule_key_value_by_type(struct vcap_rule *rule,
 					     vcap_val_to_bit(data->u1.value));
 	case VCAP_FIELD_U32:
 		return vcap_rule_add_key_u32(rule, key_id, data->u32.value,
-					     data->u32.value);
+					     data->u32.mask);
 	case VCAP_FIELD_U48:
 		return vcap_rule_add_key_u48(rule, key_id, &data->u48);
 	case VCAP_FIELD_U56:
@@ -603,7 +603,7 @@ static int vcap_genl_mod_rule_key_by_type(
 		return vcap_rule_mod_key_bit(rule, key_id, data->u1.value);
 	case VCAP_FIELD_U32:
 		return vcap_rule_mod_key_u32(rule, key_id, data->u32.value,
-					     data->u32.value);
+					     data->u32.mask);
 	case VCAP_FIELD_U48:
 		return vcap_rule_mod_key_u48(rule, key_id, &data->u48);
 	case VCAP_FIELD_U56:
