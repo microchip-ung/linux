2a98e0bdb040737489e597fad160f86fd1384805
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-02-20 15:16:25 +0100

    irqchip: Fix irqchip for jaguar2
    There were 2 issues:
    1. The offset for trigger was set wrong, it was set to 4 instead of 0x5c
    2. The offset for force was not set, it was written again the offset for
       trigger with the offset of the force register

--- drivers/irqchip/irq-mscc-ocelot.c
+++ drivers/irqchip/irq-mscc-ocelot.c
@@ -72,8 +72,8 @@ static const struct chip_props jaguar2_props = {
 	.reg_off_ena_clr	= 0x1c,
 	.reg_off_ena_set	= 0x20,
 	.reg_off_ident		= 0x38,
-	.reg_off_trigger	= 0x4,
-	.reg_off_trigger	= 0xc,
+	.reg_off_trigger	= 0x5c,
+	.reg_off_force		= 0xc,
 	.n_irq			= 29,
 };
 
