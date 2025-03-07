360cfcfd705cfcac8204ff086d3cae359c50f3d1
Ilya A. Evenbach <ievenbach@aurora.tech>
2025-03-07 11:16:01 -0800

    [sparx5] Improve reporting of failures in Sparx5i register access
    Use WARN instead of WARN_ON in register access reporting
    This lets us provide information about registers being incorrectly
    accessed

--- drivers/net/ethernet/microchip/sparx5/sparx5_main.h
+++ drivers/net/ethernet/microchip/sparx5/sparx5_main.h
@@ -1109,9 +1109,9 @@ static inline __pure int spx5_offset(int id, int tinst, int tcnt,
 				     int raddr, int rinst,
 				     int rcnt, int rwidth)
 {
-	WARN_ON((tinst) >= tcnt);
-	WARN_ON((ginst) >= gcnt);
-	WARN_ON((rinst) >= rcnt);
+	WARN((tinst) >= tcnt, "tinst %d >= tcnt %d\n", tinst, tcnt);
+	WARN((ginst) >= gcnt, "ginst %d >= gcnt %d\n", ginst, gcnt);
+	WARN((rinst) >= rcnt, "rinst %d >= rcnt %d\n", rinst, rcnt);
 	return gbase + ((ginst) * gwidth) +
 		raddr + ((rinst) * rwidth);
 }
@@ -1126,9 +1126,9 @@ static inline void __iomem *spx5_addr(void __iomem *base[],
 				      int raddr, int rinst,
 				      int rcnt, int rwidth)
 {
-	WARN_ON((tinst) >= tcnt);
-	WARN_ON((ginst) >= gcnt);
-	WARN_ON((rinst) >= rcnt);
+	WARN((tinst) >= tcnt, "tinst %d >= tcnt %d\n", tinst, tcnt);
+	WARN((ginst) >= gcnt, "ginst %d >= gcnt %d\n", ginst, gcnt);
+	WARN((rinst) >= rcnt, "rinst %d >= rcnt %d\n", rinst, rcnt);
 	return base[id + (tinst)] +
 		gbase + ((ginst) * gwidth) +
 		raddr + ((rinst) * rwidth);
@@ -1140,8 +1140,8 @@ static inline void __iomem *spx5_inst_addr(void __iomem *base,
 					   int raddr, int rinst,
 					   int rcnt, int rwidth)
 {
-	WARN_ON((ginst) >= gcnt);
-	WARN_ON((rinst) >= rcnt);
+	WARN((ginst) >= gcnt, "ginst %d >= gcnt %d\n", ginst, gcnt);
+	WARN((rinst) >= rcnt, "rinst %d >= rcnt %d\n", rinst, rcnt);
 	return base +
 		gbase + ((ginst) * gwidth) +
 		raddr + ((rinst) * rwidth);
