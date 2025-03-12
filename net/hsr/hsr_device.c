9ce3ea175cee43b838c566f74c0aa46477b4227a
Daniel Machon <daniel.machon@microchip.com>
2025-03-12 11:56:42 +0100

    net: hsr: add support for offloading redbox (HSR-SAN)
    In redbox (HSR-SAN) mode, we link three ports to the hsr master. We
    cannot tell, if the HSR driver is operating in redbox mode or not. Add a
    helper to retrieve this information, so we know whether to expect two or
    three ports.
    
    Signed-off-by: Daniel Machon <daniel.machon@microchip.com>

--- net/hsr/hsr_device.c
+++ net/hsr/hsr_device.c
@@ -785,6 +785,11 @@ int hsr_dev_finalize(struct net_device *hsr_dev, struct net_device *slave[2],
 
 	unregister = true;
 
+	/* We need to know up-front, before adding ports, if we are to be
+	 * expecting two or three ports (i.e. DANH, DANP or Redbox) */
+	if (interlink)
+		hsr->redbox = true;
+
 	res = hsr_add_port(hsr, slave[0], HSR_PT_SLAVE_A, extack);
 	if (res)
 		goto err_unregister;
@@ -798,12 +803,11 @@ int hsr_dev_finalize(struct net_device *hsr_dev, struct net_device *slave[2],
 		call_netdevice_notifiers(NETDEV_CHANGEADDR, slave[1]);
 	}
 
-	if (interlink) {
+	if (hsr->redbox) {
 		res = hsr_add_port(hsr, interlink, HSR_PT_INTERLINK, extack);
 		if (res)
 			goto err_unregister;
 
-		hsr->redbox = true;
 		ether_addr_copy(hsr->macaddress_redbox, interlink->dev_addr);
 		mod_timer(&hsr->prune_proxy_timer,
 			  jiffies + msecs_to_jiffies(PRUNE_PROXY_PERIOD));
