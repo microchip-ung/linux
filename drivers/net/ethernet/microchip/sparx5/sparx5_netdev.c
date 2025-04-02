2a2d9200719c5173807610d5a7c2f1b6906b8074
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-04-02 10:21:41 +0200

    net: lan969x: Fix support for redbox
    The redboxes are supported only on lan969x and not also on sparx5.
    Therefore make sure not to set LAN969X_SUPPORTED_HSR_FEATURES when there
    is a sparx5 chip.

--- drivers/net/ethernet/microchip/sparx5/sparx5_netdev.c
+++ drivers/net/ethernet/microchip/sparx5/sparx5_netdev.c
@@ -357,8 +357,12 @@ struct net_device *sparx5_create_netdev(struct sparx5 *sparx5, u32 portno)
 	if (!ndev)
 		return ERR_PTR(-ENOMEM);
 
-	ndev->hw_features |= (NETIF_F_HW_TC | LAN969X_SUPPORTED_HSR_FEATURES);
-	ndev->features |= (NETIF_F_HW_TC | LAN969X_SUPPORTED_HSR_FEATURES);
+	ndev->hw_features |= NETIF_F_HW_TC;
+	ndev->features |= NETIF_F_HW_TC;
+	if (!is_sparx5(sparx5)) {
+		ndev->hw_features |= LAN969X_SUPPORTED_HSR_FEATURES;
+		ndev->features |= LAN969X_SUPPORTED_HSR_FEATURES;
+	}
 	/* The MAC supports frame lengths of up to 14,000 bytes */
 	ndev->max_mtu = 14000;
 
