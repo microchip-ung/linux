8a7bd4dc9a101439c4f761bf441d562ec434077b
Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>
2025-05-06 09:08:28 +0200

    dsa: tag: lan9645x: add hsr/prp support

--- net/dsa/tag_lan9645x.c
+++ net/dsa/tag_lan9645x.c
@@ -25,8 +25,63 @@ static u8 LONG_PREFIX[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 			    0xfc, 0xff, 0xff, 0xff, 0xff, 0xff,
 			    0x88, 0x80, 0x00, 0x11 };
 
+/* The HSR header is 6 bytes, but lan9645x must align it to a 32bit boundary. This
+ * means the HSR eth type is stripped from the frame in the chip. This is
+ * automatically rectified on front-port egress, but not during CPU extraction.
+ * Lan9645x will communicate the state via the IFH in the RTAGD=0,1,2 field.
+ *
+ * We must pop the 4 byte HSR fragment before passing the frame on. If a frame
+ * is VLAN tagged, the HSR tag is always inner.
+ */
+#define HSR_FRG_HLEN 4
+
+enum {
+	HSR_MISSING = 0,
+	HSR_OUTER = 1,
+	HSR_INNER = 2,
+};
+
+static int lan9645x_pop_hsr_tag(struct sk_buff *skb, int rtagd)
+{
+	int outer_len;
+	int offset;
+	int err;
+
+	if (rtagd == HSR_MISSING)
+		return 0;
+
+	offset = skb->data - skb_mac_header(skb);
+
+	if (WARN_ONCE(offset,
+		      "%s got skb with skb->data not at mac header (offset %d)\n",
+		      __func__, offset)) {
+		return -EINVAL;
+	}
+
+	outer_len = rtagd == HSR_INNER ? VLAN_HLEN : 0;
+
+	err = skb_ensure_writable(skb, ETH_HLEN + outer_len + HSR_FRG_HLEN);
+	if (unlikely(err))
+		return err;
+
+	skb_postpull_rcsum(skb, skb->data + (2 * ETH_ALEN + outer_len),
+			   HSR_FRG_HLEN);
+
+	memmove(skb->data + HSR_FRG_HLEN, skb->data, 2 * ETH_ALEN + outer_len);
+	skb_pull(skb, HSR_FRG_HLEN);
+
+	skb_reset_mac_header(skb);
+
+	if (skb_network_offset(skb) < ETH_HLEN)
+		skb_set_network_header(skb, ETH_HLEN);
+
+	skb_reset_mac_len(skb);
+
+	return err;
+}
+
 static void lan9645x_rcv_dbg(struct sk_buff *skb, struct net_device *ndev,
-			   u8 *ifh, u32 ifh_gap_len)
+			     u8 *ifh, u32 ifh_gap_len)
 {
 	u64 seqnum, cpuq, flood, dd, rct, rtagd, popcnt, etype_ofs, acl_id,
 		acl_hit;
@@ -64,12 +119,20 @@ static bool lan9645x_is_mld(struct sk_buff *skb)
 	       ipv6_mc_check_mld(skb);
 }
 
-static void lan9645x_offload_fwd_mark(struct sk_buff *skb)
+static void lan9645x_offload_fwd_mark(struct sk_buff *skb, u32 rtagd)
 {
 	/* IGMP/MLD are trapped to CPU, and must be forwarded by network stack.
 	 */
 	if (!ip_mc_check_igmp(skb) || lan9645x_is_mld(skb)) {
 		skb->offload_fwd_mark = 0;
+		return;
+	}
+
+	if (rtagd > 0) {
+		/* Not strictly necessary due to the fwd offload flag in the HSR
+		 * driver.
+		 */
+		skb->offload_fwd_mark = !!(rtagd > 0);
 		return;
 	}
 
@@ -90,11 +153,11 @@ static struct sk_buff *lan9645x_xmit(struct sk_buff *skb, struct net_device *nde
 	BUILD_BUG_ON(ARRAY_SIZE(LONG_PREFIX) != LAN9645X_LONG_PREFIX_LEN);
 
 	lan9645x_xmit_get_vlan_info(skb, dsa_port_bridge_dev_get(dp), &vlan_tci,
-				  &tag_type);
+				    &tag_type);
 
 	qos_class = netdev_get_num_tc(ndev) ?
-			    netdev_get_prio_tc_map(ndev, skb->priority) :
-			    skb->priority;
+		netdev_get_prio_tc_map(ndev, skb->priority) :
+		skb->priority;
 
 	/* Make room for IFH */
 	ifh = skb_push(skb, LAN9645X_IFH_LEN);
@@ -104,27 +167,76 @@ static struct sk_buff *lan9645x_xmit(struct sk_buff *skb, struct net_device *nde
 	long_prefix = skb_push(skb, ARRAY_SIZE(LONG_PREFIX));
 	memcpy(long_prefix, LONG_PREFIX, ARRAY_SIZE(LONG_PREFIX));
 
-	LAN9645X_IFH_SET(ifh, IFH_BYPASS, 1);
-	LAN9645X_IFH_SET(ifh, IFH_SRCPORT, cpu_port);
-	LAN9645X_IFH_SET(ifh, IFH_QOS_CLASS, qos_class);
-	LAN9645X_IFH_SET(ifh, IFH_TCI, vlan_tci);
-	LAN9645X_IFH_SET(ifh, IFH_TAG_TYPE, tag_type);
-	LAN9645X_IFH_SET(ifh, IFH_DSTS, BIT_ULL(dp->index));
-	netdev_dbg(ndev, "xmit: dst=%u\n", dp->index);
+	if (dp->hsr_dev) {
+		/* At the moment the HSR driver does not implement special
+		 * handling of PRP San nodes. The standard suggests that DANP
+		 * nodes which maintain a nodetable should:
+		 * Xmit frames to SAN nodes only on the LAN(s) where it is
+		 * detected, without RCT.
+		 *
+		 * If a SAN moves between A and B, frames must be xmitted on both
+		 * for the duration of NodeForgetTime, after which frames are
+		 * once again only xmitted on new LAN.
+		 *
+		 * At the moment RCT is always added, and frame is sent out on
+		 * both A and B, which causes a lot of known UC flood noise on
+		 * the LAN where the node does not exist.
+		 *
+		 * IF this is implemented, we will likely have a bug here. We set
+		 * the NETIF_F_HW_HSR_DUP flag, and lan9645x can offload
+		 * duplication, but only for PRP frames with RCT, not SAN nodes,
+		 * since lan9645x can not offload the node table.
+		 *
+		 * If an skb is meant for a SAN, and hsr driver did not prepare
+		 * the RCT, then we want the BYPASS=1 logic below.
+		 *
+		 * At the moment we will get two kinds of bugs:
+		 *
+		 * 1) This branch is taken, and we set BYPASS=0 and RCT_INJ=1.
+		 * This tells lan9645x that the tail of the frame is a prepared
+		 * RCT, so it will insert seq number and lanid, leading to a
+		 * potentially corrupted frame, unless padding bytes were
+		 * modified.
+		 *
+		 * 2) The HSR driver will only xmit the frame on one interface,
+		 * since we set the DUP flag. But for these SAN frames the HW
+		 * does not offload duplication.
+		 *
+		 * Handling this in the tag driver is a bit delicate, since we
+		 * only get the SKB, and does not have access to the decisions
+		 * made in the HSR driver.
+		 */
+		LAN9645X_IFH_SET(ifh, IFH_BYPASS, 0);
+		/* When bypass=0 the IFH is just thrown away by the port.
+		 * However, a few fields get speciel treatment by the port, and
+		 * are passed to the IFH created in the analyzer.
+		 */
+		LAN9645X_IFH_SET(ifh, IFH_MASQ, 1);
+		LAN9645X_IFH_SET(ifh, IFH_MASQ_PORT, cpu_port);
+		LAN9645X_IFH_SET(ifh, IFH_RCT_INJ, 1);
+		LAN9645X_IFH_SET(ifh, IFH_SRCPORT, cpu_port);
+	} else {
+		LAN9645X_IFH_SET(ifh, IFH_BYPASS, 1);
+		LAN9645X_IFH_SET(ifh, IFH_SRCPORT, cpu_port);
+		LAN9645X_IFH_SET(ifh, IFH_QOS_CLASS, qos_class);
+		LAN9645X_IFH_SET(ifh, IFH_TCI, vlan_tci);
+		LAN9645X_IFH_SET(ifh, IFH_TAG_TYPE, tag_type);
+		LAN9645X_IFH_SET(ifh, IFH_DSTS, BIT_ULL(dp->index));
+	}
 
 	return skb;
 }
 
 static struct sk_buff *lan9645x_rcv(struct sk_buff *skb, struct net_device *ndev)
 {
+	u64 vlan_tci, tag_type, popcnt, etype_ofs;
+	u64 src_port, qos_class, rtagd, rct;
 	u8 *orig_skb_data = skb->data;
-	u64 src_port, qos_class;
-	u64 vlan_tci, tag_type;
-	u64 popcnt, etype_ofs;
 	struct dsa_port *dp;
 	u32 ifh_gap_len = 0;
 	u16 vlan_tpid;
 	u8 *ifh;
+	int err;
 
 	/* DSA master already consumed DMAC,SMAC,ETYPE from long prefix. Go back
 	 * to beginning of frame.
@@ -139,6 +251,8 @@ static struct sk_buff *lan9645x_rcv(struct sk_buff *skb, struct net_device *ndev
 	vlan_tci = LAN9645X_IFH_GET(ifh, IFH_TCI);
 	popcnt = LAN9645X_IFH_GET(ifh, IFH_POP_CNT);
 	etype_ofs = LAN9645X_IFH_GET(ifh, IFH_ETYPE_OFS);
+	rct = LAN9645X_IFH_GET(ifh, IFH_RCT_AVAIL);
+	rtagd = LAN9645X_IFH_GET(ifh, IFH_RTAGD);
 
 	/* Set skb->data at start of real header
 	 *
@@ -146,11 +260,11 @@ static struct sk_buff *lan9645x_rcv(struct sk_buff *skb, struct net_device *ndev
 	 * need to account for any tags popped by the hardware, as that will
 	 * leave a gap between the IFH and DMAC.
 	 */
-	if (popcnt == 0 && etype_ofs == 0) {
+	if (popcnt == 0 && etype_ofs == 0)
 		ifh_gap_len = 2 * VLAN_HLEN;
-	} else if (popcnt == 3) {
+	else if (popcnt == 3)
 		ifh_gap_len = VLAN_HLEN;
-	}
+
 	skb_pull(skb, LAN9645X_IFH_LEN + ifh_gap_len);
 	skb_reset_mac_header(skb);
 	skb_reset_mac_len(skb);
@@ -159,6 +273,24 @@ static struct sk_buff *lan9645x_rcv(struct sk_buff *skb, struct net_device *ndev
 	skb_pull(skb, ETH_HLEN);
 	skb_postpull_rcsum(skb, orig_skb_data,
 			   LAN9645X_TOTAL_TAG_LEN + ifh_gap_len);
+
+	if (rtagd > 0) {
+		skb_push_rcsum(skb, ETH_HLEN);
+		err = lan9645x_pop_hsr_tag(skb, rtagd);
+		if (err) {
+			dev_err(&ndev->dev, "%s: HSR tag pop error=%d",
+				__func__, err);
+			return NULL;
+		}
+		skb_pull_rcsum(skb, ETH_HLEN);
+	} else if (rct) {
+		err = pskb_trim_rcsum(skb, skb->len - HSR_HLEN);
+		if (err) {
+			dev_err(&ndev->dev, "%s: PRP RCT trailer trim err=%d",
+				__func__, err);
+			return NULL;
+		}
+	}
 
 	lan9645x_rcv_dbg(skb, ndev, ifh, ifh_gap_len);
 
@@ -171,7 +303,7 @@ static struct sk_buff *lan9645x_rcv(struct sk_buff *skb, struct net_device *ndev
 		return NULL;
 	}
 
-	lan9645x_offload_fwd_mark(skb);
+	lan9645x_offload_fwd_mark(skb, rtagd);
 
 	skb->priority = qos_class;
 
@@ -213,5 +345,5 @@ static const struct dsa_device_ops lan9645x_netdev_ops = {
 
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS_DSA_TAG_DRIVER(DSA_TAG_PROTO_LAN9645X, LAN9645X_NAME);
-
+MODULE_AUTHOR("Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>");
 module_dsa_tag_driver(lan9645x_netdev_ops);
