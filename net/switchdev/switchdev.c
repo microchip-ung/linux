c75e910ae3b24c2ccff1b8c94ceb6431c4a13f1f
Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>
2025-05-27 14:34:12 +0200

    net: switchdev: fix dsa conduit in handle_port_{attr,obj}* helpers
    If a switchdev device is used as a CPU host for a DSA device, and the
    switchdev device uses the switchdev_handle_port_obj* helpers, then a bug
    arises due to DSA conduit interfaces being lower interfaces of all DSA
    ports.
    
    If a dsa user port is added/deleted to an MDB group, then the action is
    reflected on the conduit interface.
    
    bridge mdb add dev br0 port swp1 grp G
    bridge mdb add dev br0 port swp2 grp G
    
    bridge mdb del dev br0 port swp2 grp G
    bridge mdb del dev br0 port swp1 grp G
    
    Here the switchdev device will get mdb port add/leave events twice for
    the conduit interface. This will break the behaviour, with specific
    failure mode depending on how the switchdev device reponds to these
    duplicated events.
    
    Likewise, for __switchdev_handle_port_attr_* a bridge with VLAN
    filtering 1, will make the conduit vlan aware and can cause tag
    push/popping etc.

--- net/switchdev/switchdev.c
+++ net/switchdev/switchdev.c
@@ -18,6 +18,7 @@
 #include <linux/if_vlan.h>
 #include <linux/rtnetlink.h>
 #include <net/switchdev.h>
+#include <net/dsa.h>
 
 static bool switchdev_obj_eq(const struct switchdev_obj *a,
 			     const struct switchdev_obj *b)
@@ -730,6 +731,26 @@ int switchdev_handle_fdb_event_to_device(struct net_device *dev, unsigned long e
 }
 EXPORT_SYMBOL_GPL(switchdev_handle_fdb_event_to_device);
 
+static bool switchdev_lower_is_dsa_conduit(struct net_device *dev,
+					   struct net_device *lower_dev)
+{
+#if IS_ENABLED(CONFIG_NET_DSA)
+	struct dsa_port *dp;
+
+	if (!netdev_uses_dsa(lower_dev))
+		return false;
+
+	dp = dsa_port_from_netdev(dev);
+
+	if (IS_ERR_OR_NULL(dp))
+		return false;
+
+	return dsa_port_to_conduit(dp) == lower_dev;
+#else
+	return false;
+#endif
+}
+
 static int __switchdev_handle_port_obj_add(struct net_device *dev,
 			struct switchdev_notifier_port_obj_info *port_obj_info,
 			bool (*check_cb)(const struct net_device *dev),
@@ -763,6 +784,9 @@ static int __switchdev_handle_port_obj_add(struct net_device *dev,
 	 */
 	netdev_for_each_lower_dev(dev, lower_dev, iter) {
 		if (netif_is_bridge_master(lower_dev))
+			continue;
+
+		if (switchdev_lower_is_dsa_conduit(dev, lower_dev))
 			continue;
 
 		/* When searching for switchdev interfaces that are neighbors
@@ -876,6 +900,9 @@ static int __switchdev_handle_port_obj_del(struct net_device *dev,
 		if (netif_is_bridge_master(lower_dev))
 			continue;
 
+		if (switchdev_lower_is_dsa_conduit(dev, lower_dev))
+			continue;
+
 		/* When searching for switchdev interfaces that are neighbors
 		 * of foreign ones, and @dev is a bridge, do not recurse on the
 		 * foreign interface again, it was already visited.
@@ -985,6 +1012,9 @@ static int __switchdev_handle_port_attr_set(struct net_device *dev,
 	 */
 	netdev_for_each_lower_dev(dev, lower_dev, iter) {
 		if (netif_is_bridge_master(lower_dev))
+			continue;
+
+		if (switchdev_lower_is_dsa_conduit(dev, lower_dev))
 			continue;
 
 		err = __switchdev_handle_port_attr_set(lower_dev, port_attr_info,
@@ -1115,6 +1145,9 @@ static int __switchdev_handle_port_attr_get(struct net_device *dev,
 	 */
 	netdev_for_each_lower_dev(dev, lower_dev, iter) {
 		if (netif_is_bridge_master(lower_dev))
+			continue;
+
+		if (switchdev_lower_is_dsa_conduit(dev, lower_dev))
 			continue;
 
 		err = __switchdev_handle_port_attr_get(lower_dev, port_attr_info,
