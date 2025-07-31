17e67a64a763fdb83171177da9495f4925055549
Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>
2025-07-31 10:03:00 +0200

    dsa: lan9645x: fix license

--- include/linux/dsa/lan9645x.h
+++ include/linux/dsa/lan9645x.h
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0
-/* Copyright (C) 2025 Microchip Technology Inc.
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2025 Microchip Technology Inc.
  */
 
 #ifndef _NET_DSA_TAG_LAN9645X_H_
