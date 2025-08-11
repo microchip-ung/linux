ac8d94098b1edb440a9b324abd26bc28e86a44d4
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-08-11 21:36:21 +0200

    net: sparx5: Fix the check for speed
    The interfaces PHY_INTERFACE_MODE_SGMII and PHY_INTERFACE_MODE_QSGMII
    support speeds up to 1G, but the check was also for 2.5G which is wrong.
    Fix this by checking only the supported speed.

--- drivers/net/ethernet/microchip/sparx5/sparx5_port.c
+++ drivers/net/ethernet/microchip/sparx5/sparx5_port.c
@@ -281,6 +281,11 @@ static int sparx5_port_verify_speed(struct sparx5 *sparx5,
 			return sparx5_port_error(port, conf, SPX5_PERR_IFTYPE);
 		fallthrough;
 	case PHY_INTERFACE_MODE_SGMII:
+		if (conf->speed != SPEED_1000 &&
+		    conf->speed != SPEED_100 &&
+		    conf->speed != SPEED_10)
+			return sparx5_port_error(port, conf, SPX5_PERR_SPEED);
+		break;
 	case PHY_INTERFACE_MODE_10G_QXGMII:
 		if (conf->speed != SPEED_1000 &&
 		    conf->speed != SPEED_100 &&
