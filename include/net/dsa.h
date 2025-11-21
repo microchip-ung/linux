1221e564b3fd903024a4bdfc86d096350552417c
Horatiu Vultur <horatiu.vultur@microchip.com>
2025-11-21 15:16:24 +0100

    net: dsa: add port_rxtstamp_all
    This function will be called for all non-ptp event frames.
    In this way the driver can see all the frames and read their ingress
    timestamps.

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -1153,6 +1153,8 @@ struct dsa_switch_ops {
 				 struct sk_buff *skb);
 	bool	(*port_rxtstamp)(struct dsa_switch *ds, int port,
 				 struct sk_buff *skb, unsigned int type);
+	bool	(*port_rxtstamp_all)(struct dsa_switch *ds, int port,
+				     struct sk_buff *skb, unsigned int type);
 
 	/* Devlink parameters, etc */
 	int	(*devlink_param_get)(struct dsa_switch *ds, u32 id,
