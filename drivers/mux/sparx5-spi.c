c785ed1539a57d7edd4af528c9be224c8d68dcd2
Horatiu Vultur <horatiu.vultur@microchip.com>
2026-03-02 14:00:32 +0100

    mux: sparx5-spi: Add MODULE_LICENSE and MODULE_DESCRIPTION
    If the sparx5-spi is built as a kernel module then the build will throw
    a warning or an error depending on what is missing:
    
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/mux/mux-sparx5.o
    
    ERROR: modpost: missing MODULE_LICENSE() in drivers/mux/mux-sparx5.o
    
    Adding the MODULE_DESCRIPTION() and MODULE_LICENSE() seems to fix these issues.

--- drivers/mux/sparx5-spi.c
+++ drivers/mux/sparx5-spi.c
@@ -137,3 +137,6 @@ static struct platform_driver mux_sparx5_driver = {
 	.probe = mux_sparx5_probe,
 };
 module_platform_driver(mux_sparx5_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Microchip sparx5 SPI mux");
