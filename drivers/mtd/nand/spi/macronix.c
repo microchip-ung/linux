05327e886b712afce35cc0162088a4a6a1ffa659
Steen Hegelund <steen.hegelund@microchip.com>
2026-03-05 11:11:36 +0100

    mtd: spi-nand: macronix: Remove SPINAND_HAS_PROG_PLANE_SELECT_BIT for MX35LF2G24AD (DBB-1102)
    Fixes: https://jira.microchip.com/browse/DBB-1102

--- drivers/mtd/nand/spi/macronix.c
+++ drivers/mtd/nand/spi/macronix.c
@@ -219,8 +219,7 @@ static const struct spinand_info macronix_spinand_table[] = {
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
 					      &write_cache_variants,
 					      &update_cache_variants),
-		     SPINAND_HAS_QE_BIT |
-		     SPINAND_HAS_PROG_PLANE_SELECT_BIT,
+		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout, NULL),
 		     SPINAND_READ_RETRY(MACRONIX_NUM_READ_RETRY_MODES,
 					macronix_set_read_retry)),
